031aec1c9b0f7ce873c97f3103375aeaccc384b8
Danny <a.ovalle@alumno.um.edu.ar>
2025-04-01 20:53:40 -0300

    DB conection finished

--- Main/admin.py
+++ Main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Pedido, DetallePedido, Pago
 
 # Register your models here.
 
 admin.site.register(Producto)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
+admin.site.register(Pago)
--- TechWave/settings.py
+++ TechWave/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'TechWave.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techwavedb',  
+        'USER': 'root',         
+        'PASSWORD': 'admin',  
+        'HOST': 'localhost',          
+        'PORT': '3306',               
+        'OPTIONS': {
+            'charset': 'utf8mb4',  
+        }
     }
 }
 
